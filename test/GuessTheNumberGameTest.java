4c27c9e69d6a3e7c5265c5158f33114e077ca3ce
lauradefreitas2 <lauradovalegameplays@gmail.com>
2023-11-21 11:31:03 -0300

    finalização do projeto

--- test/GuessTheNumberGameTest.java
+++ test/GuessTheNumberGameTest.java
@@ -1,19 +1,21 @@
 package test;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import src.ComputerPlayer;
 import src.GuessTheNumberGame;
 import src.HumanPlayer;
 import src.Player;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class GuessTheNumberGameTest {
     @Test
     public void testHumanPlayerMakeGuess() {
-        HumanPlayer player = new HumanPlayer("Fulana");
-
-        player.setManualGuess(42);
+        HumanPlayer player = Mockito.mock(HumanPlayer.class);
+        when(player.makeGuess()).thenReturn(42);
 
         int guess = player.makeGuess();
 
@@ -22,7 +24,8 @@ public class GuessTheNumberGameTest {
 
     @Test
     public void testComputerPlayerMakeGuess() {
-        ComputerPlayer player = new ComputerPlayer("Computer");
+        ComputerPlayer player = Mockito.mock(ComputerPlayer.class);
+        when(player.makeGuess()).thenReturn(50);
 
         int guess = player.makeGuess();
 
@@ -34,12 +37,34 @@ public class GuessTheNumberGameTest {
         GuessTheNumberGame game = new GuessTheNumberGame();
         game.inicializarJogo();
 
-        Player jogadorHumano = new HumanPlayer("Jogador", 50);
-        Player jogadorComputador = new ComputerPlayer("Computador");
+
+        HumanPlayer jogadorHumano = Mockito.mock(HumanPlayer.class);
+        when(jogadorHumano.makeGuess()).thenReturn(50);
+
+        ComputerPlayer jogadorComputador = Mockito.mock(ComputerPlayer.class);
+        when(jogadorComputador.makeGuess()).thenReturn(30);
+
         int maxTentativas = 10;
         int result = game.jogarJogo(jogadorHumano, jogadorComputador, maxTentativas);
         assertTrue(result == 0 || result == 1);
     }
+    @Test
+    public void testGamePlayMaxAttemptsReached() {
+        GuessTheNumberGame game = new GuessTheNumberGame();
+        game.inicializarJogo();
+
+        Player jogadorHumano = Mockito.mock(HumanPlayer.class);
+        when(jogadorHumano.makeGuess()).thenReturn(30);
+
+        Player jogadorComputador = Mockito.mock(ComputerPlayer.class);
+        when(jogadorComputador.makeGuess()).thenReturn(40);
+
+        int maxTentativas = 1;
+        int result = game.jogarJogo(jogadorHumano, jogadorComputador, maxTentativas);
+
+        assertEquals(1, result);
+    }
+
 
 }
 
